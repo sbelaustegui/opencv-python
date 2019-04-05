7336c85faf3fae9e06d7e385b7b0daf121818830
Angeles Chiang <maria.chiang@ing.austral.edu.ar>
2019-04-05 16:44:01 -0300

    modified geometric transformation 2.) done geometric transformation 3.)

--- clase3_tp/geometric_transformation/g_t_2.py
+++ clase3_tp/geometric_transformation/g_t_2.py
@@ -12,22 +12,26 @@ matrix = cv.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
 img2 = cv.warpAffine(img, matrix, (cols, rows))
 cv.imshow("image", img)
 cv.imshow("image2", img2)
-posList = []
+distList = []
 
 
+# read 3 inputs and draw circles
 def draw_circle(event, x, y, flags, param):
-   global posList, posNp
+   global distList, dist
    if event == cv.EVENT_LBUTTONDOWN:
-        if len(posList) <= 2:
+        if len(distList) <= 2:
             cv.circle(img, (x, y), 5, (0, 0, 255), -1)
             cv.circle(img2, (x, y), 5, (0, 0, 255), -1)
-            posList.append((x, y))
-            print posList
+            distList.append((x, y))
+            print distList
             cv.imshow("image", img)
             cv.imshow("image2", img2)
-            posNp = np.float32(posList)  # convert to numpy for other usages
-        elif len(posList) == 3:
-            dist = np.float32([[0, 0], [cols, 0], [0, rows]])
+            dist = np.float32(distList)  # convert to numpy for other usages
+        elif len(distList) == 3:
+            # first point = top-left
+            # second point = top-right
+            # third point = bottom-left
+            posNp = np.float32([[0, 0], [cols, 0], [0, rows]])  # points set to corners
             new_matrix = cv.getAffineTransform(posNp, dist)
             result1 = cv.warpAffine(img, new_matrix, (cols, rows))
             result2 = cv.warpAffine(img2, new_matrix, (cols, rows))
