810c361454948ccb47c49bf82d8390bb197b6212
sbelaustegui <sebasbelaustegui@hotmail.com>
2019-04-22 19:17:49 -0300

    tp4 added

--- clase4_tp/backgroundSubtractor.py
+++ clase4_tp/backgroundSubtractor.py
@@ -1,34 +1,48 @@
-import cv2 as cv
 import numpy as np
+import cv2
+import sys
 import time as tm
 
-slider_val = -100
-slider_val_max = 100
+video_path = 'carsRt9_3.avi'
+cv2.ocl.setUseOpenCL(False)
 
 
-def on_trackbar(val):
-    global slider_val
-    slider_val = val
+#read video file
+cap = cv2.VideoCapture(video_path)
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 
-cap = cv.VideoCapture("carsRt9_3.avi")
-fgbg = cv.createBackgroundSubtractorMOG2(history=20, varThreshold=25, detectShadows=True)
+while (cap.isOpened):
 
-while True:
+    #if ret is true than no error with cap.isOpened
     ret, frame = cap.read()
-    cv.imshow("origin", frame)
-    fgmask = fgbg.apply(frame)
-    cv.imshow("fgmask", fgmask)
-    tm.sleep(0.1)
-    bgImg = fgbg.getBackgroundImage()
-    cv.imshow("backgroundImage", bgImg)
-    cv.createTrackbar("Trackbar", "fgbg2", slider_val, slider_val_max, on_trackbar)
-    fgbg2 = cv.BackgroundSubtractor.apply(fgbg, bgImg, fgmask, slider_val/100)
-    cv.imshow("fgbg2", fgbg2)
 
-    if cv.waitKey(1) & 0xFF == 27:
-        break
+    if ret==True:
+
+        #apply background substraction
+        fgmask = fgbg.apply(frame)
+
+        (contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+
+        #looping for contours
+        for c in contours:
+            if cv2.contourArea(c) < 500:
+                continue
+
+            #get bounding box from countour
+            (x, y, w, h) = cv2.boundingRect(c)
+
+            #draw bounding box
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow('foreground and background',fgmask)
+        tm.sleep(0.05)
+        cv2.imshow('rgb',frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
